96fb996549872419b985e71d6982ec8bf992e192
Whalter Duarte <dev@whalter.com.br>
2024-01-09 12:16:51 -0300

    Rotas de post curso e de get all adicionadas

--- src/controller/cursos.controller.ts
+++ src/controller/cursos.controller.ts
@@ -136,32 +136,22 @@ export const listCategory = async (req: Request, res: Response) => {
   //POST SUB-CATEGORIA
   export const addSubcategory = async (req: Request, res: Response) => {
     // Aceita apenas imagens
-    if (req.file) {
-      const file = req.file as UploadedFile;
-    } else {
+    if (!req.file) {
       res.status(404).json({ error: 'Arquivo não enviado' });
       return;
     }
   
-    const { slugSubCategory, title, categoriaId } = req.body;
+    const { title, categoriaId } = req.body;
     const photo: string = `${process.env.BASE}/${(req.file as Express.MulterS3.File)?.key}`;
   
     try {
-      // Verifica se a subcategoria já existe
-      const existingCategory = await prisma.subcategoria.findFirst({
-        where: {
-          slugSubCategory: slugSubCategory,
-        },
-      });
-  
-      if (existingCategory) {
-        return res.status(409).json({ error: 'Subcategoria já existente' });
-      }
-  
       // Verifica se todos os campos necessários estão preenchidos
-      if (!slugSubCategory || !title || !categoriaId) {
+      if (!title || !categoriaId) {
         return res.status(404).json({ error: 'Preencha todos os campos obrigatórios' });
       }
+  
+      // Gera um slug único
+      const slug = await generateUniqueSlugSubcategory(title);
   
       // Converte categoriaId para um número
       const categoriaIdNumber = parseInt(categoriaId, 10);
@@ -169,7 +159,7 @@ export const listCategory = async (req: Request, res: Response) => {
       // Cria uma nova subcategoria associada à categoria especificada
       const newSubcategory = await prisma.subcategoria.create({
         data: {
-          slugSubCategory,
+          slugSubCategory: slug,
           title,
           photo,
           categoriaId: categoriaIdNumber,
@@ -183,35 +173,174 @@ export const listCategory = async (req: Request, res: Response) => {
     }
   };
   
+  // Função para gerar slugs únicos (verifica se já existe no banco de dados)
+  async function generateUniqueSlugSubcategory(title: string): Promise<string> {
+    if (!title) {
+      console.error('Erro ao gerar slug: o título não pode ser vazio ou undefined.');
+      return ''; // Ou outra estratégia apropriada para lidar com títulos vazios ou indefinidos
+    }
+  
+    let slug = title.toLowerCase().replace(/\s+/g, '-');
+  
+    // Verifica se o slug já existe no banco de dados
+    const existingSubcategory = await prisma.subcategoria.findFirst({
+      where: {
+        slugSubCategory: slug,
+      },
+    });
+  
+    // Adiciona um sufixo ao slug se já existir
+    if (existingSubcategory) {
+      let counter = 1;
+      while (true) {
+        const newSlug = `${slug}-${counter}`;
+        const existing = await prisma.subcategoria.findFirst({
+          where: {
+            slugSubCategory: newSlug,
+          },
+        });
+        if (!existing) {
+          slug = newSlug;
+          break;
+        }
+        counter++;
+      }
+    }
+  
+    return slug;
+  }
+  
   // CURSOS/////////////////
 
-  export const listCourses = async (req: Request, res: Response) => {
-   
-  };
+ // Lista todos os cursos
+export const listCourses = async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const pageSize = 5; 
+
+  try {
+    const courses = await prisma.curso.findMany({
+      take: pageSize,
+      skip: (page - 1) * pageSize,
+      orderBy: { createdAt: 'desc' },
+    });
+
+    return res.json({ courses });
+  } catch (error) {
+    console.error('Erro ao listar cursos:', error);
+    return res.status(500).json({ error: 'Erro ao listar cursos' });
+  }
+};
+
+// Recupera detalhes de um curso específico
+export const getCourse = async (req: Request, res: Response) => {
+  const { slugCursos } = req.params;
+
+  try {
+    const course = await prisma.curso.findFirst({
+      where: {
+        slugCursos,
+      },
+    });
+
+    if (!course) {
+      return res.status(404).json({ status: false, message: 'Nenhum curso encontrado' });
+    }
+
+    return res.json({ status: true, course });
+  } catch (error) {
+    console.error('Erro ao obter curso:', error);
+    return res.status(500).json({ error: 'Erro ao obter curso' });
+  }
+};
 
   export const Courses = async (req: Request, res: Response) => {
-   
+    return res.json({})
   };
   
   //ADD CURSO
- export const addCourse = async (req: Request, res: Response) => {
-  // Aceita apenas imagens
-  if (req.file) {
-    const file = req.file as UploadedFile;
-  } else {
-    res.status(404).json({ error: 'Arquivo não enviado' });
-    return;
+  export const addCourse = async (req: Request, res: Response) => {
+    // Aceita apenas vídeos
+    if (!req.file) {
+      res.status(404).json({ error: 'Arquivo não enviado' });
+      return;
+    }
+  
+    const { subcategoriaId, title } = req.body;
+    const video: string = `${process.env.BASE}/${(req.file as Express.MulterS3.File)?.key}`;
+  
+    try {
+      // Verifica se a subcategoria existe
+      const subcategoriaIdNumber = parseInt(subcategoriaId, 10);
+  
+      const existingSubcategory = await prisma.subcategoria.findUnique({
+        where: {
+          id: subcategoriaIdNumber,
+        },
+      });
+  
+      if (!existingSubcategory) {
+        return res.status(404).json({ error: 'Subcategoria não encontrada' });
+      }
+  
+      // Verifica se o título foi fornecido
+      if (!title) {
+        return res.status(400).json({ error: 'O título do curso é obrigatório' });
+      }
+  
+      // Gera um slug único
+      const uniqueSlug = await generateUniqueSlug(title);
+  
+      // Cria um novo curso associado à subcategoria especificada
+      const newCourse = await prisma.curso.create({
+        data: {
+          slugCursos: uniqueSlug,
+          title,
+          file: video,
+          subcategoriaId: subcategoriaIdNumber,
+        },
+      });
+  
+      return res.status(201).json({ course: newCourse });
+    } catch (error) {
+      console.error('Erro ao adicionar curso:', error);
+      return res.status(500).json({ error: 'Erro ao adicionar curso' });
+    }
+  };
+  
+  // Função para gerar slugs únicos (verifica se já existe no banco de dados)
+  async function generateUniqueSlug(title: string): Promise<string> {
+    if (!title) {
+      console.error('Erro ao gerar slug: o título não pode ser vazio ou undefined.');
+      return ''; // Ou outra estratégia apropriada para lidar com títulos vazios ou indefinidos
+    }
+  
+    let slug = title.toLowerCase().replace(/\s+/g, '-');
+  
+    // Verifica se o slug já existe no banco de dados
+    const existingCourse = await prisma.curso.findFirst({
+      where: {
+        slugCursos: slug,
+      },
+    });
+  
+    // Adiciona um sufixo ao slug se já existir
+    if (existingCourse) {
+      let counter = 1;
+      while (true) {
+        const newSlug = `${slug}-${counter}`;
+        const existing = await prisma.curso.findFirst({
+          where: {
+            slugCursos: newSlug,
+          },
+        });
+        if (!existing) {
+          slug = newSlug;
+          break;
+        }
+        counter++;
+      }
+    }
+  
+    return slug;
   }
-
-  const { subcategoriaId, title } = req.body;
-  const video: string = `${process.env.BASE}/${(req.file as Express.MulterS3.File)?.key}`;
-
-  try {
-
-    
-    return res.status(201).json({ });
-  } catch (error) {
-    console.error('Erro ao adicionar curso:', error);
-    return res.status(500).json({ error: 'Erro ao adicionar curso' });
-  }
-};
+  
