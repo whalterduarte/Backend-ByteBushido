4cd579e1100bcca34f1045932aea229772b702f5
Whalter Duarte <dev@whalter.com.br>
2024-01-23 10:55:53 -0300

    Pagina not found

--- app.ts
+++ app.ts
@@ -1,37 +1,35 @@
-import express, {Request, Response}  from "express"
-import login from './src/routes/login.routes'
-import cursos from './src/routes/cursos.routes'
-import bodyParser from 'body-parser'
-import path from 'path'
-import cors from "cors"
+import express, { Request, Response } from "express";
+import login from "./src/routes/login.routes";
+import cursos from "./src/routes/cursos.routes";
+import bodyParser from "body-parser";
+import path from "path";
+import cors from "cors";
 
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
 
-  // Configuração do CORS
-  const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions));
+// Configuração do CORS
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
- //Body Parser
- app.use(bodyParser.urlencoded({extended: false}))
- app.use(bodyParser.json())
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
- //Rotas
-   //Home
- app.use('/', login)
- app.use('/cursos', cursos)
-
+//Rotas
+//Home
+app.use("/", login);
+app.use("/cursos", cursos);
 
 //Para rotas não encotradas
-app.use((req: Request, res:Response)=>{
-  res.status(404).send('Pagina não encontrada')
-})
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ Server: "Pagina não encontrada" });
+});
 
-app.listen(port, () => console.log(`Servidor rodando na porta : ${port}!`))
-
+app.listen(port, () => console.log(`Servidor rodando na porta : ${port}!`));
